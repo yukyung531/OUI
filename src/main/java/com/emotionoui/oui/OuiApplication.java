7fca5e4da13360a17a1cdeb1ee9d0050b165d175
정민지 <mjje27@gmail.com>
2024-03-11 17:00:23 +0900

    feat: Init Spring Project

---
+++ src/main/java/com/emotionoui/oui/OuiApplication.java
@@ -0,0 +1,13 @@
+package com.emotionoui.oui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OuiApplication.class, args);
+    }
+
+}
