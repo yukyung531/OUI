b5d9872f0bcb7a3c8eea5d31285d23ae50748993
현민수 <gusalstn91@ajou.ac.kr>
2024-03-19 22:08:20 +0900

    modify: Add Monthly graph
    Former-commit-id: 2e67f7154c893ab5e0ad0eaaf1ca6b1cc9880865

---
+++ frontend/src/pages/analysis/components/Monthly/Monthly.tsx
@@ -0,0 +1,86 @@
+import { Doughnut } from "react-chartjs-2";
+import angry from 'src/asset/images/emotion/angry.png';
+import embarrass from 'src/asset/images/emotion/embarrass.png';
+import joy from 'src/asset/images/emotion/joy.png';
+import nervous from 'src/asset/images/emotion/nervous.png';
+import relax from 'src/asset/images/emotion/relax.png';
+import sad from 'src/asset/images/emotion/sad.png';
+import styled from "@emotion/styled";
+
+
+const IconWrapper = styled.div`
+  max-width: 1024px;
+  display: flex;
+  flex-direction: row; 
+  justify-content: space-evenly; 
+  flex-wrap: wrap; 
+  margin: auto; 
+
+  img, div {
+    flex: 1 1 auto;
+    max-width: 100px; 
+    margin: 10px; 
+    height: auto; 
+
+    @media (max-width: 768px) {
+      max-width: 50px; 
+    }
+  }
+`;
+
+const Monthly = () => {
+
+    const images = {
+        sad: sad,
+        nervous: nervous,
+        angry: angry,
+        embarrass: embarrass,
+        relax: relax,
+        joy: joy,
+    };
+
+    const colors = {
+        sad: '#C0DEFF',
+        nervous: '#BDB5FF',
+        angry: '#F09690',
+        embarrass: '#BBDED6',
+        relax: '#FFC814',
+        joy: '#FFDD6B',
+    }
+
+
+    const imageArray = Object.values(images);
+
+    const Data = {
+        labels: [ '슬픔', '불안', '분노', '당황', '느긋', '기쁨'],
+        datasets: [
+          {
+            data: [35, 5, 20, 10, 10, 40],
+            fill: true,
+            backgroundColor: Object.values(colors),
+            borderColor: 'transparent',
+          }
+        ]
+    };
+
+    return(
+        <>
+            <div style={{ fontFamily: 'IMHyeMin', fontWeight: 'bold', fontSize: '16px' }}>
+                공유일 님이 3월에 느낀 “감정 통계” 예요!
+            </div>
+            <div>
+                <Doughnut data={ Data }></Doughnut>
+            </div>
+            <IconWrapper>
+                <>
+                {imageArray .map(( image, index ) => (
+                    <img src = { image } />
+                    
+                ))}
+                </>
+            </IconWrapper>
+        </>
+        
+    );
+}
+export default Monthly;
--- frontend/src/pages/research/index.tsx
+++ frontend/src/pages/research/index.tsx
@@ -1,17 +1,24 @@
 import { Box } from "@mui/material";
+import music from 'src/asset/images/research/research.png';
+import happy from 'src/asset/images/research/happyIcon.png';
+import sad from 'src/asset/images/research/sadIcon.png';
+import happyText from 'src/asset/images/research/searchHappy.png';
+import sadText from 'src/asset/images/research/searchSad.png';
+import question from 'src/asset/images/research/searchQuestion.png';
 import styled from "styled-components";
+
 
 const BoxWrapper = styled(Box)`
   border: 1px solid black;
   width: 70%;
-  margin: auto; // 중앙 정렬을 위해 추가
-  display: flex; // 오타 수정
+  margin: auto; 
+  display: flex;
   justify-content: center;
   align-items: center;
-  flex-wrap: wrap; // 내용이 넘칠 경우 줄 바꿈
+  flex-wrap: wrap;
   margin: 2vh;
   @media (max-width: 768px) {
-    flex-direction: column; // 화면이 작을 때 세로 방향으로 요소들을 배열
+    flex-direction: column;
   }
 `;
 
@@ -24,28 +31,22 @@ const QuestionWrapper = styled.div`
 `;
 
 const Research = () => {
-  const music = `${process.env.PUBLIC_URL}/images/research.png`;
-  const happy = `${process.env.PUBLIC_URL}/images/happyIcon.png`;
-  const sad = `${process.env.PUBLIC_URL}/images/sadIcon.png`;
-  const happyText = `${process.env.PUBLIC_URL}/images/searchHappy.png`;
-  const sadText = `${process.env.PUBLIC_URL}/images/searchSad.png`;
-  const question = `${process.env.PUBLIC_URL}/images/searchQuestion.png`;
 
   return (
     <>
       <QuestionWrapper>
-        <img src={music} alt="music" style={{ maxWidth: "100%", height: "auto", margin: '10vh' }} />
+        <img src={ music } alt="music" style={{ maxWidth: "100%", height: "auto", margin: '10vh' }} />
         <BoxWrapper>
-          <img src={sad} alt="Sad" style={{ maxWidth: "45%", height: "auto", margin: "10px" }} />
-          <img src={sadText} alt="sadText" style={{ maxWidth: "45%", height: "auto", margin: "10px" }} />
+          <img src={ sad } alt="Sad" style={{ maxWidth: "45%", height: "auto", margin: "10px" }} />
+          <img src={ sadText } alt="sadText" style={{ maxWidth: "45%", height: "auto", margin: "10px" }} />
         </BoxWrapper>
         <BoxWrapper>
-          <img src={happy} alt="happy" style={{ maxWidth: "45%", height: "auto", margin: "10px" }} />
-          <img src={happyText} alt="happyText" style={{ maxWidth: "45%", height: "auto", margin: "10px" }} />
+          <img src={ happy } alt="happy" style={{ maxWidth: "45%", height: "auto", margin: "10px" }} />
+          <img src={ happyText } alt="happyText" style={{ maxWidth: "45%", height: "auto", margin: "10px" }} />
         </BoxWrapper>   
       </QuestionWrapper>
 
-      <img src={question} alt="question" style={{ maxWidth: "40%", height: "auto" }} />
+      <img src={ question } alt="question" style={{ maxWidth: "40%", height: "auto" }} />
     </>
   );
 }
