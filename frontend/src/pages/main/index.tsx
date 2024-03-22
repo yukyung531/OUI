e18d4b714bc5d385def8702e8edc06758ca644b2
현민수 <gusalstn91@ajou.ac.kr>
2024-03-23 00:49:30 +0900

    modify: change mainpage css
    Former-commit-id: 048e83afe167e6f2daf437612b25b27a2f696702

--- frontend/src/pages/main/index.tsx
+++ frontend/src/pages/main/index.tsx
@@ -1,8 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Card } from "src/pages/main/components/Card";
 import { Header } from "src/components/control/Header";
-import { BottomNavi } from "src/components/control/BottomNavi";
-import { Drawer } from "src/components/control/Drawer";
 import { Button } from "src/components";
 import { CustomModal } from "./components/Modal";
 import { getDiary } from './api/getDiary';
@@ -15,7 +13,6 @@ import styled from "styled-components";
 
 const SliderWrapper = styled( Slider )`
   
-
   .slick-track{
     display: flex;
     margin: 0 -10px;
@@ -32,6 +29,13 @@ const SliderWrapper = styled( Slider )`
     transition: transform 0.5s ease, opacity 0.5s ease;
   }
 
+  .slick-slide > div { 
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    background-position: center;
+  }
+
   .slick-center {
     transform: scale(1); 
     opacity: 1;
@@ -40,7 +44,59 @@ const SliderWrapper = styled( Slider )`
     justifyContent: center; 
     z-index: 10;
   }
+
+  .slick-slide:not(.slick-center) > div {
+    background-image: none !important; 
+  }
+
 `;
+
+
+const YellowBox = styled.div`
+
+  width: 500px;
+  height: 8vh;
+  background-color: #FFE17D;
+  border-radius: 10px;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
+  display: flex;
+  justify-content: center;
+  margin-top: 4vh;
+  margin-bottom: 4vh;
+  align-items: center;
+  margin-left: 5%;
+  @media (max-width: 768px) {
+    margin-left: 2%; 
+    width: 50vw;
+  }
+
+  @media (max-width: 480px) {
+    margin-left: 1%; 
+    width: 50vw;
+    height: 4vh;
+  }
+  position: relative;
+`;
+
+const UserRecord = styled.div`
+  position: absolute;
+  top: -40px; 
+  background: transparent;
+  padding: 5px 10px;
+  border: 0px;
+  font-size: 60px;
+  font-family: "Dovemayo",
+  @media (max-width: 768px) {
+    font-size: 35px;
+    top: -30px; 
+  }
+
+  @media (max-width: 480px) {
+    font-size: 20px;
+    top: -20px; 
+  }
+`;
+
 
 const Main = () => {
 
@@ -58,64 +114,85 @@ const Main = () => {
   const [ isModalOpen, setIsModalOpen ] = useState( false );
   const [ diaryCount, setDiaryCount ] = useState(0); 
   const [ diaryList, setDiaryList ] = useState([]);
+  const [userName, setUserName] = useState("");
+
   const openModal = () => setIsModalOpen( true );
   const closeModal = () => {
     console.log( '모달 닫기!!!!!!' );
     setIsModalOpen( false );
   }
 
-  const [cards, setCards] = useState([
-    { id: 1, buttonText: "1", isDiary: "diary", type: '개인' },
-    { id: 2, buttonText: "2", isDiary: "diary" , type: '개인'},
-    { id: 3, buttonText: "3", isDiary: "diary", type: '개인' },
-    { id: 4, buttonText: "4", isDiary: "diary", type: '공유' },
-    { id: 5, buttonText: "카드 추가", isDiary: "addButton" },
-  ]);
 
-
-  const addCard = ( props:addProps ) => { // 카드 추가 완료
-    console.log(props);
+  const addCard = ( props: { title: any; key: any; members: any; } ) => {
     const { title, key, members } = props;
-
-    const newCardNumber = cards.length; 
-    const newCards = cards.slice( 0, -1 ); 
-    const newCard = { id: newCardNumber, buttonText: `${ key }`, isDiary: "diary" };
-    const addButtonCard = cards[ cards.length - 1 ]; 
-    setCards([ ...newCards, newCard, addButtonCard ]);
-    console.log( '추가 완료!!!!!' );
+    const newEntry = { id: diaryList.length + 1, title, key, members };
+    setDiaryList([ ...diaryList, newEntry ]);
     closeModal();
   };
 
-  const moveDiary = (type) => {
+  const cards = diaryList.map(( diary ) => ({
+    id: diary.diaryId,
+    buttonText: diary.createdAt,
+    isDiary: "diary",
+    template: diary.templateId,
+    type: diary.type,
+  })).concat({
+    id: diaryList.length,
+    buttonText: "카드 추가",
+    isDiary: "addButton",
+    template: -1,
+    type: "",
+  });
+
+  const moveDiary = ( type: string, id: number ) => {
     type=='개인' && 
-    navigator('/calendar', {state : {diaryId: 1, type: '개인'}})
+    navigator('/calendar', {state : {diaryId: id, type: type}})
     type=='공유' && 
-    navigator('/calendar', {state : {diaryId: 3, type: '공유'}})
+    navigator('/calendar', {state : {diaryId: id, type: type}})
   }
+
+  const { data, refetch } = useQuery([ 'diaryData' ], getDiary, {
+    onSuccess: (res) => {
+      const updatedDiaryList = res.data.map(( diary: { createdAt: any[]; }) => ({
+        ...diary,
+        createdAt: `${ diary.createdAt[0] }.${ diary.createdAt[1] }.${ diary.createdAt[2] }`,
+      }));
+      setDiaryList( updatedDiaryList );
+      setUserName( res.data[0].memberId ); 
+      console.log( res.data );
+    }
+  });
+
+  useEffect(() => {
+    refetch();
+  }, [ refetch ]);
 
 
   return (
     <>
     <Header>
-    <Drawer></Drawer>
-    <Button path='/diary/write' btType='user' name="temp"></Button>
+    <Button path='/diary/write' btType='bell' name="temp"></Button>
     </Header>
-    <hr></hr>
+    <hr/>
+    <YellowBox>
+        {userName && <UserRecord style={{ fontFamily: 'IMHyeMin', fontWeight: 'bold' }}>{ userName }님의 감정기록 :)</UserRecord>}
+    </YellowBox>
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <div className="slider-container" style={{ minHeight: '100%', minWidth: '100%' }}>      
         <SliderWrapper { ...settings }>
         {cards.map(( card, index ) => (
           <div key={index}>
-            <Card buttonText={ card.buttonText } onClick={ card.isDiary === "addButton" ? openModal : () => moveDiary(card?.type) } />
+            <Card buttonText={ card.buttonText } templateId={ card.template } 
+              onClick={ card.isDiary === "addButton" ? openModal : () => moveDiary( card?.type, card.id ) } />
           </div> 
         ))}
+          <Card/>
           <Card/>
           <Card/>
         </SliderWrapper>
       </div>
     </div>
     <CustomModal isOpen={ isModalOpen } closeModal={ closeModal } isFinish={ addCard }></CustomModal>
-    <BottomNavi></BottomNavi>
     </>
   );
 }
