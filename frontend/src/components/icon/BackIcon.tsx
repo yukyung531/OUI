7826d179580e63c2442c019e208a63cc063d4a76
sunyoung315 <rlatjsdud315@gmail.com>
2024-03-25 00:37:29 +0900

    refactor: 코드 리팩토링
    Former-commit-id: aa0d00163c18843b717920a388898885e2f75f5c

--- frontend/src/components/icon/BackIcon.tsx
+++ frontend/src/components/icon/BackIcon.tsx
@@ -4,20 +4,20 @@ const ButtonWrapper = styled.button<{ size: number}>`
     box-sizing: border-box;
     display: flex;
     width: ${( props ) => props.size }px;
-    padding: 0px;
-    margin: 20px 50px;
     border: none;
     cursor: pointer;
     background-color: transparent;
 `
 
 const BackIcon = ( props: IconProps ) => {
-    const { size = 70, onClick } = props;
+    const { size , onClick } = props;
 
     return (
         <ButtonWrapper size={ size } onClick={ onClick }>
-            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 17 14" fill="none">
-                <path d="M7 1L1 7M1 7L7 13M1 7H16" stroke="#222222" />
+            <svg xmlns="http://www.w3.org/2000/svg" width="45" height="46" viewBox="0 0 45 46" fill="none">
+                <rect width="29.2566" height="5" rx="2.75" transform="matrix(0.651531 -0.758622 0.643764 0.765224 0.330078 22.4141)" fill="#262626"/>
+                <rect width="29.2895" height="5" rx="2.75" transform="matrix(0.646303 0.763081 -0.649001 0.760787 4.27344 19.2451)" fill="#262626"/>
+                <rect x="4.66992" y="19.7207" width="40" height="5" rx="2.75" fill="#262626"/>
             </svg>
         </ButtonWrapper>
     )
@@ -26,6 +26,6 @@ const BackIcon = ( props: IconProps ) => {
 export default BackIcon;
 
 type IconProps = {
-  size?: number,
-  onClick?: () => void,
+    size?: number,
+    onClick?: () => void,
 }
---
+++ frontend/src/pages/diary/components/BottomSheet.tsx
@@ -0,0 +1,103 @@
+import { useState } from 'react';
+import { TextboxContent, ImageContent, DrawingContent } from '../components';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  margin: auto; 
+  max-width: 1024px; 
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: white;
+  transition: transform 0.3s ease-in-out;
+  box-shadow: 0 -3px 10px rgba(0, 0, 0, 0.1);
+  border-radius: 20px 20px 0 0;
+`;
+
+const Header = styled.div`
+  height: 60px;
+  padding: 10px 30px;
+  background-color: white;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  border-bottom: 1px solid lightgray;
+`;
+
+const Tab = styled.button`
+    box-sizing: border-box;
+    display: inline-block;
+    border: none;
+    cursor: pointer;
+    background-color: transparent;
+    font-size: 24px;
+`
+
+const Content = styled.div`
+  max-height: 300px;
+  padding: 16px;
+  overflow-y: auto;
+`;
+
+const BottomSheet = ( props: BottomSheetProps ) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const { activeTool, setActiveTool, canvas, textboxRef, textboxProps, isDeco } = props;
+
+  return (
+    <Container style={{ transform: isOpen ? 'translateY(0)' : 'translateY(calc(100% - 130px))' }}>
+        <div style={{ textAlign: "center", padding: "8px 0 4px 0" }}>
+          {isOpen && (
+            <Tab onClick={() => setIsOpen(false)}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="158" height="17" viewBox="0 0 158 17" fill="none">
+                <path d="M4 4L79 12.3333L154 4" stroke="#CDCDCD" strokeWidth="7" strokeLinecap="round"/>
+              </svg>
+            </Tab>
+          )}
+          {!isOpen && (
+            <Tab onClick={() => setIsOpen(true)}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="158" height="17" viewBox="0 0 158 17" fill="none">
+                <path d="M4 13L79 4.67L154 13" stroke="#CDCDCD" strokeWidth="7" strokeLinecap="round"/>
+              </svg>
+            </Tab>
+          )}
+        </div>
+      <Header>
+        {!isDeco && (
+          <Tab onClick={ () => setActiveTool("textbox") } disabled={ activeTool === "textbox" }>텍스트</Tab>
+        )}
+        <Tab onClick={ () => setActiveTool("image") } disabled={ activeTool === "image" }>이미지</Tab>
+        <Tab onClick={ () => setActiveTool("drawing") } disabled={ activeTool === "drawing" }>그리기</Tab>
+        <Tab onClick={ () => setActiveTool('eraser') } disabled={ activeTool === "eraser" }>지우개</Tab>
+      </Header>
+      <Content>
+        {(activeTool === "textbox") && (
+          <TextboxContent canvas={ canvas } textboxRef={ textboxRef } textboxProps={ textboxProps } />
+        )}
+        {(activeTool === "image") && (
+          <ImageContent canvas={ canvas } />
+        )}
+        {(activeTool === "drawing" || activeTool === "eraser") && (
+          <DrawingContent canvas={ canvas } />
+        )}
+      </Content>
+    </Container>
+  );
+};
+
+export default BottomSheet;
+
+type BottomSheetProps = {
+  activeTool: string,
+  setActiveTool: React.Dispatch<React.SetStateAction<string>>,
+  canvas: fabric.Canvas,
+  textboxRef?: React.MutableRefObject<fabric.Textbox>,
+  textboxProps?: {
+    selectedFont: string;
+    fontWeight: string;
+    textAlign: string;
+    fontColor: string;
+  },
+  isDeco?: boolean,
+}
