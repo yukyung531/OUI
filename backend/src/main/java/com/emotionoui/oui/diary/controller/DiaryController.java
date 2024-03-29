5b1d11d91938be4184e957984a380f118c2d8d79
yukyung531 <yukyung531@gmail.com>
2024-03-29 10:03:13 +0900

    modify:: 날짜로 해당 날짜 ë일기 존재 유무 확인

--- backend/src/main/java/com/emotionoui/oui/diary/controller/DiaryController.java
+++ backend/src/main/java/com/emotionoui/oui/diary/controller/DiaryController.java
@@ -72,7 +72,7 @@ public class DiaryController {
     // 일기 게시글 날짜로 조회하기
     @GetMapping("/{diaryId}/{date}")
     public ResponseEntity<?> searchDailyDiary(@PathVariable("diaryId") Integer diaryId, @PathVariable("date") String date, @AuthenticationPrincipal Member member){
-        return new ResponseEntity<SearchDailyDiaryRes>(diaryService.searchDailyDiaryByDate(diaryId, date, member.getMemberId()), HttpStatus.OK);
+        return new ResponseEntity<Boolean>(diaryService.searchDailyDiaryByDate(diaryId, date, member.getMemberId()), HttpStatus.OK);
     }
 
     // 감정분석 결과 보여주기
