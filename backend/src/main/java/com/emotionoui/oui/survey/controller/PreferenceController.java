9460083e9d4432597035cca5ddfeb0cd6af0d9b5
조권호 <whrnjsgh12@naver.com>
2024-04-02 05:44:58 +0900

    feat: add preference typeApi

--- backend/src/main/java/com/emotionoui/oui/survey/controller/PreferenceController.java
+++ backend/src/main/java/com/emotionoui/oui/survey/controller/PreferenceController.java
@@ -6,6 +6,7 @@ import com.emotionoui.oui.survey.dto.req.PreferenceReq;
 import com.emotionoui.oui.survey.entity.Preference;
 import com.emotionoui.oui.survey.service.PreferenceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -29,4 +30,9 @@ public class PreferenceController {
         return preferenceService.isExisted(member);
     }
 
+    @GetMapping("/type")
+    public ResponseEntity<?> getType(@AuthenticationPrincipal Member member){
+        String type =  preferenceService.getTypeByMemberId(member);
+        return new ResponseEntity<String>(type, HttpStatus.OK);
+    }
 }
