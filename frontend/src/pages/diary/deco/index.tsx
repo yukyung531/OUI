bb8c6b02a43c3c1b2ba15a9d3322d297063f7e3f
sunyoung315 <rlatjsdud315@gmail.com>
2024-04-01 22:26:20 +0900

    feat: 웹소켓 연결 및 음악 플레이어 UI 수정
    Former-commit-id: 9d7992b4373c0e716e410e613c05f8563c7f4c64

--- frontend/src/pages/diary/deco/index.tsx
+++ frontend/src/pages/diary/deco/index.tsx
@@ -6,9 +6,9 @@ import { useEffect, useState, useRef } from 'react';
 import { useMutation, useQuery } from 'react-query';
 import useStore from 'src/store';
 import { getDiary, postDiaryDeco } from '../api';
-// import { Stomp } from "@stomp/stompjs";
-// import SockJS from "sockjs-client";
-// import { v4 as uuidv4 } from 'uuid';
+import { Stomp } from "@stomp/stompjs";
+import SockJS from "sockjs-client";
+import { v4 as uuidv4 } from 'uuid';
 import styled from 'styled-components';
 
 const Header = styled.div`
@@ -32,15 +32,14 @@ const DiaryDeco = () => {
     const { dailyDiaryId, type } = useStore();
 
     const canvasRef = useRef(null);
-    // let stompClient = useRef(null);
-    // let memberId = useRef('');
-    // let activeId = useRef(null);
+    let stompClient = useRef(null);
+    let memberId = useRef('');
     
     const [ canvas, setCanvas ] = useState(null);
     const [ isFontLoaded, setIsFontLoaded ] = useState(false);
     const [ activeTool, setActiveTool ] = useState("image");
     const [ isLoadingDiary, setIsLoadingDiary ] = useState(false);
-    // const [ isConnected, setIsConnected ] = useState(false);
+    const [ isConnected, setIsConnected ] = useState(false);
     
     const { data: dailyDiary } = useQuery('dailyDiary', () => getDiary(dailyDiaryId), {
         enabled: isFontLoaded
@@ -73,117 +72,193 @@ const DiaryDeco = () => {
         setIsLoadingDiary(true);
     }, [ dailyDiary, isLoadingDiary ]);
         
-//     const storedDataString = localStorage.getItem('userStorage');
-//     const storedData = JSON.parse(storedDataString);
-//     const accessToken = storedData?.state?.accessToken;
+    const storedDataString = localStorage.getItem('userStorage');
+    const storedData = JSON.parse(storedDataString);
+    const accessToken = storedData?.state?.accessToken;
 
-//     useEffect(() => {
-//         if(!isLoadingDiary) return;
+    useEffect(() => {
+        if(!isLoadingDiary) return;
 
-//         const socket = new SockJS(process.env.REACT_APP_BASE_URL + '/ws');
-//         stompClient.current = Stomp.over(socket);
+        const socket = new SockJS(process.env.REACT_APP_BASE_URL + '/ws');
+        stompClient.current = Stomp.over(socket);
         
-//         stompClient.current.connect(
-//             {
-//                 Authorization : `Bearer ${ accessToken }`,
-//             }, 
-//             (frame) => {
-//                 setIsConnected(true);
-//                 memberId.current = frame.headers['user-name'];
+        stompClient.current.connect(
+            {
+                Authorization : `Bearer ${ accessToken }`,
+            }, 
+            (frame) => {
+                setIsConnected(true);
+                memberId.current = frame.headers['user-name'];
 
-//                 stompClient.current.subscribe(`/sub/decorate/${dailyDiaryId}`, (resp) => {
-//                     const object = JSON.parse(JSON.parse(resp.body).body.decoration);
+                stompClient.current.subscribe(`/sub/decorate/${dailyDiaryId}`, (resp) => {
+                    const object = JSON.parse(JSON.parse(resp.body).body.decoration);
                     
-//                     const objectMemberId = JSON.parse(JSON.parse(resp.body).body.decoration).memberId;
-//                     const action = JSON.parse(JSON.parse(resp.body).body.decoration).action;
+                    const objectMemberId = JSON.parse(JSON.parse(resp.body).body.decoration).memberId;
+                    const action = JSON.parse(JSON.parse(resp.body).body.decoration).action;
+
+                    const existContent = canvas.getObjects().some(obj => obj.id === object.id);
                     
-//                     // 1. 캔버스에 객체가 추가될 때,
-//                     if(action === 'add' && (memberId.current !== objectMemberId )) {
-//                         const objectId = object.id;
-//                         const objectMemberId = object.memberId;
-//                         const objectAction = object.action;
-//                         fabric.util.enlivenObjects([object], (enlivenedObjects) => {
-//                             enlivenedObjects.forEach((obj) => {
-//                                 obj.set({
-//                                     id: objectId,
-//                                     memberId: objectMemberId,
-//                                     action: objectAction,
-//                                 })
-//                                 canvas.add(obj);
-//                                 console.log(canvas.getObjects());
-//                             });
-//                             canvas.renderAll();
-//                         }, null);
-//                     }
-//                     // 2. 객체가 삭제될 때,
-//                     else if(action === 'remove') {
-//                         // allObjects.forEach((obj) => {
-//                         //     console.log(obj.id)
-//                         //     if(obj.id === object.id) {
-//                         //         console.log('remove object id', obj.id);
-//                         //         canvas.remove(obj);
-//                         //     }
-//                         // })
-//                     } 
-//                 });
-//             });
+                    // 1. 캔버스에 객체가 추가될 때,
+                    if(action === 'add' && (memberId.current !== objectMemberId)) {
+                        const objectId = object.id;
+                        const objectAction = object.action;
+
+                        if(!existContent) {
+                            fabric.util.enlivenObjects([object], (enlivenedObjects) => {
+                                enlivenedObjects.forEach((obj) => {
+                                    obj.set({
+                                        id: objectId,
+                                        action: objectAction,
+                                    })
+                                    canvas.add(obj);
+                                    console.log(canvas.getObjects());
+                                });
+                                canvas.renderAll();
+                            }, null);
+                        }
+                    }
+                    // 2. 객체가 삭제될 때,
+                    else if(action === 'remove') {
+                        canvas.getObjects().forEach((obj) => {
+                            if(obj.id === object.id) {
+                                canvas.remove(obj);
+                            }
+                        })
+                    } 
+                    // 3. 객체가 이동될 때,
+                    else if(action === 'moving') {
+                        const obj = canvas.getObjects().find(o => o.id === object.id);
+
+                        if(obj && (object.memberId !== memberId.current)) {
+                            obj.set({
+                                id: object.id,
+                                left: object.left,
+                                top: object.top,
+                                scaleX: object.scaleX,
+                                scaleY: object.scaleY,
+                            });
+                            canvas.renderAll();
+                        }
+                    } 
+                    // 4. 객체를 수정할 때,
+                    else if(action === 'modify') {
+                        const obj = canvas.getObjects().find(o => o.id === object.id);
+
+                        if(obj) {
+                            obj.set({
+                                id: object.id,
+                                angle: object.angle,
+                                width: object.width,
+                                height: object.height,
+                                left: object.left,
+                                top: object.top,
+                                scaleX: object.scaleX,
+                                scaleY: object.scaleY,
+                            })
+                            canvas.renderAll();
+                        }
+                    }
+                });
+            });
             
-//         return () => {
-//             stompClient.current.disconnect();
-//             setIsConnected(false);
-//         };
-//     }, [ isLoadingDiary ]); 
+        return () => {
+            stompClient.current.disconnect();
+            setIsConnected(false);
+        };
+    }, [ isLoadingDiary ]); 
    
-//     const handleObjectAdded = (event) => {
-//         const obj = event.target;
-//         const jsonObj = obj.toJSON(['id', 'action', 'memberId']);
-//         if(!jsonObj.id) {
-//             jsonObj.id = uuidv4();
-//             jsonObj.memberId = memberId.current;
-//             jsonObj.action = 'add';
-
-//             const addedObject = JSON.stringify(jsonObj);
-    
-//             stompClient.current.send(
-//                 `/pub/decorate/${dailyDiaryId}`,
-//                 {},
-//                 JSON.stringify({decoration: addedObject}),
-//             );
-//         }
-//     };
-
-//     const handleObjectRemoved = (event) => {
-//         const obj = event.target;
-//         const jsonObj = obj.toJSON(['action']);
-//         jsonObj.action ='remove';
-//         const removedObject = JSON.stringify(jsonObj);
-
-//         stompClient.current.send(
-//             `/pub/decorate/${dailyDiaryId}`,
-//             {},
-//             JSON.stringify({decoration: removedObject}),
-//         );
-//     };
-
-//     const handleObjectMoving = (event) => {
-//         const jsonObj = event.target.toJSON(['id', 'action','memberId']);
+    const handleObjectAdded = (event) => {
+        const obj = event.target;
         
-//         console.log('pub move: ', jsonObj);
-//     }
-       
-//    useEffect(() => {
-//         if (!canvas || !isConnected) return;
+        const jsonObj = obj.toJSON(['id', 'action', 'memberId']);
+        jsonObj.action = 'add';
+        if(!jsonObj.id) {
+            jsonObj.id = uuidv4();
+            obj.set('id', jsonObj.id);
+        }
 
-//         canvas.on('object:added', handleObjectAdded);
-//         canvas.on('object:moving', handleObjectMoving);
-//         canvas.on('object:removed', handleObjectRemoved);
+        const addedObject = JSON.stringify(jsonObj);
+
+        stompClient.current.send(
+            `/pub/decorate/${dailyDiaryId}`,
+            {},
+            JSON.stringify({decoration: addedObject}),
+        );
+    };
+
+    const handleObjectRemoved = (event) => {
+        const obj = event.target;
+
+        const jsonObj = obj.toJSON(['action', 'id', 'memberId']);
+        jsonObj.action = 'remove';
+        jsonObj.id = obj.id;
+
+        const removedObject = JSON.stringify(jsonObj);
+
+        stompClient.current.send(
+            `/pub/decorate/${dailyDiaryId}`,
+            {},
+            JSON.stringify({decoration: removedObject}),
+        );
+    };    
+
+    const handleObjectMoving = (event) => {
+        const obj = event.target;
+
+        const jsonObj = obj.toJSON(['id', 'action','memberId']);
+        jsonObj.memberId = memberId.current;
+        jsonObj.action = 'moving';
+        if(!jsonObj.id) {
+            jsonObj.id = uuidv4();
+            obj.set({
+                'id': jsonObj.id,
+                'memberId': jsonObj.memberId
+            });
+        } else {
+            jsonObj.id = obj.id;
+        }
+
+        const movingObject = JSON.stringify(jsonObj);
         
-//         return () => {
-//             canvas.off('object:added', handleObjectAdded);
-//             canvas.off('object:moving', handleObjectMoving);
-//             canvas.off('object:removed', handleObjectRemoved);
-//         };
-//    }, [ isConnected, canvas ]); 
+        stompClient.current.send(
+            `/pub/decorate/${dailyDiaryId}`,
+            {},
+            JSON.stringify({decoration: movingObject}),
+        );
+    }
+
+    const handleObjectModified = (event) => {
+        const obj = event.target;
+
+        const jsonObj = obj.toJSON(['id', 'action','memberId']);
+        jsonObj.memberId = memberId.current;
+        jsonObj.action ='modify';
+        jsonObj.id = obj.id;
+
+        const modifyObject = JSON.stringify(jsonObj);
+
+        stompClient.current.send(
+            `/pub/decorate/${dailyDiaryId}`,
+            {},
+            JSON.stringify({decoration: modifyObject}),
+        );
+    }
+
+    useEffect(() => {
+        if (!canvas || !isConnected) return;
+
+        canvas.on('object:added', handleObjectAdded);
+        canvas.on('object:removed', handleObjectRemoved);
+        canvas.on('object:moving', handleObjectMoving);
+        canvas.on('object:modified', handleObjectModified);
+        
+        return () => {
+            canvas.off('object:added', handleObjectAdded);
+            canvas.off('object:removed', handleObjectRemoved);
+            canvas.off('object:moving', handleObjectMoving);
+            canvas.off('object:modified', handleObjectModified);
+        };
+    }, [ isConnected, canvas ]); 
 
     const decoDiary = useMutation( postDiaryDeco );
 
