01e343123138bbe7447b06e877b4b71bcb3accda
조권호 <whrnjsgh12@naver.com>
2024-04-02 05:43:58 +0900

    feat: add myPage
    Former-commit-id: c13f0ef04635b6876bcfed21d9257bb32240c95b

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,12 @@ import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import routers from './pages/router'
-import useStore from './store'
+import { createPortal } from 'react-dom'
 import { CookiesProvider } from 'react-cookie';
 import { StyledEngineProvider } from '@mui/material/styles';
 import styled from 'styled-components';
+import useStore from 'src/store'
+import { MyPage } from 'src/components/control'
 
 
 const AppWrapper = styled.div`
@@ -15,14 +17,55 @@ const AppWrapper = styled.div`
   width: 100%; 
   min-height: 100vh;
   background-color: #F9F3EE;
-`;
+`
+const ModalBackground = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 400;
+`
+const Modal = styled.div`
+  height: 50%;
+  width: 60%;
+  background-color: #FFF;
+  box-shadow: 0px -3px 0px 0px rgba(211, 211, 211, 0.2);
+  border-radius: 10px;
+  position: fixed;
+  padding: 10px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`
 
 const queryClient = new QueryClient() // QueryClient 생성
 
 function App() {
 
+  const { isModalOpened, updateModal } = useStore()
   
   const [ router ] = useState( createBrowserRouter( routers ))
+  
+  const html = document.querySelector( 'html' )
+
+  const closeModal = () => { 
+      updateModal();
+      html?.classList.remove( 'scroll-locked' )
+  }
+
+  const ModalPortal = ({ children, onClose  }) => { 
+    const handleBackgroundClick = (e) => {
+      ( e.target === e.currentTarget ) && onClose()
+    }
+    return createPortal(
+      <ModalBackground onClick={ handleBackgroundClick }>
+        <Modal>{ children }</Modal>
+      </ModalBackground>,
+      document.body
+    )
+  }
+
 
   return (
     <CookiesProvider>
@@ -31,6 +74,12 @@ function App() {
         <StyledEngineProvider injectFirst>
           <AppWrapper>
             <RouterProvider router={ router } />
+            { 
+              isModalOpened && 
+                <ModalPortal onClose={ closeModal }>
+                  <MyPage/>
+                </ModalPortal>
+            }
           </AppWrapper>
         </StyledEngineProvider>
       </Suspense>
--- frontend/src/components/control/BottomNavi/BottomNavi.tsx
+++ frontend/src/components/control/BottomNavi/BottomNavi.tsx
@@ -5,6 +5,7 @@ import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined
 import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
 import MainLogo from 'src/asset/images/image-icon/logo.png';
 import { useNavigate } from 'react-router-dom';
+import useStore from 'src/store'
 import styled from 'styled-components';
 import AlarmModal from 'src/components/modal/AlarmModal';
 
@@ -23,7 +24,9 @@ const BottomNaviWrapper = styled( BottomNavigation )`
 
 export default function BottomNavi() {
   const [value, setValue] = useState(0);
-  const [isModalOpen, setIsModalOpen] = useState( false );
+
+  const { isModalOpened, updateModal } = useStore()
+  const [ isModalOpen, setIsModalOpen ] = useState( false );
   const navigator = useNavigate();
 
   const goMain = () => {
@@ -32,7 +35,11 @@ export default function BottomNavi() {
 
   const toggleModal = () => { 
     setIsModalOpen( !isModalOpen );
-  };
+  }
+  
+  const viewMyPage = () =>{
+    updateModal()
+  }
 
   return (
     <>
@@ -43,7 +50,7 @@ export default function BottomNavi() {
           setValue( newValue );
         }}
       >
-        <BottomNavigationAction icon={<PersonOutlineOutlinedIcon />} />
+        <BottomNavigationAction icon={<PersonOutlineOutlinedIcon />} onClick={ viewMyPage } />
         <BottomNavigationAction icon={<img src={ MainLogo } style={{ width: 24, height: 24 }} onClick={ goMain } />} />
         <BottomNavigationAction icon={<NotificationsNoneOutlinedIcon />} onClick={ toggleModal } />
       </BottomNaviWrapper>
