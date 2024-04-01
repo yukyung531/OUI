685b6ee395c3b7edfdc000c5007feea7a5c7bb74
조권호 <whrnjsgh12@naver.com>
2024-04-01 10:47:08 +0900

    modify: corsMapping

--- backend/src/main/java/com/emotionoui/oui/common/config/CorsConfig.java
+++ backend/src/main/java/com/emotionoui/oui/common/config/CorsConfig.java
@@ -12,7 +12,8 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**") // 모든 경로에 대해 CORS 허용
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE","OPTIONS", "HEAD", "PATCH")
-                .exposedHeaders("Authorization", "RefreshToken", "Content-Type");
-//                .allowCredentials(true); // 인증 정보 허용 설정
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization", "RefreshToken", "Content-Type")
+                .allowCredentials(true); // 인증 정보 허용 설정
     }
 }
