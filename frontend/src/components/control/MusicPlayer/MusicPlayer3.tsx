873a76a92d63574fa03028b62db7360a3ef4753a
현민수 <gusalstn91@ajou.ac.kr>
2024-04-03 00:19:52 +0900

    modify: add music title
    Former-commit-id: 9419b34a67556dde6e8efc449ebb8a2876c02147

--- frontend/src/components/control/MusicPlayer/MusicPlayer3.tsx
+++ frontend/src/components/control/MusicPlayer/MusicPlayer3.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, SetStateAction } from 'react';
+import { useState, useRef, SetStateAction, useEffect } from 'react';
 import PauseIcon from 'src/asset/images/icon/pause.svg';
 import NextIcon from 'src/asset/images/icon/next.svg';
 import PrevIcon from 'src/asset/images/icon/prev.svg';
@@ -77,30 +77,26 @@ const Thumbnail = styled.img`
   margin-right: 5px; 
 `;
 
-const playlist = [
-  'https://www.youtube.com/watch?v=xzsRBKZh1No',
-  'https://www.youtube.com/watch?v=yL6P7OR5WOM',
-  'https://www.youtube.com/watch?v=Hfep-HihqZo',
-  'https://www.youtube.com/watch?v=k3-BDy55tq4'
-];
 
-const getThumbnailUrl = ( url:string ) => {
-  const videoId = url.split('v=')[1];
-  return `https://img.youtube.com/vi/${ videoId }/default.jpg`;
-}
+// 
+
+
 
 const AudioVideoPlayer = ( props: MusicListProps ) => {
 
   const { playList } = props;
-
+  
   const [playing, setPlaying] = useState( false );
   const [trackIndex, setTrackIndex] = useState(0);
   const [played, setPlayed] = useState(0);
+  const [ recommends, setRecommends ] = useState([])
   const playerRef = useRef( null );
+  const [ Title, setTitle ] = useState('');
+  const [ Singer, setSinger ] = useState('');
 
   const togglePlayPause = () => setPlaying( !playing );
-  const playPreviousTrack = () => setTrackIndex(( prevIndex ) => ( prevIndex === 0 ? playlist.length - 1 : prevIndex - 1 ));
-  const playNextTrack = () => setTrackIndex(( prevIndex ) => ( prevIndex === playlist.length - 1 ? 0 : prevIndex + 1 ));
+  const playPreviousTrack = () => setTrackIndex(( prevIndex ) => ( prevIndex === 0 ? recommends.length - 1 : prevIndex - 1 ));
+  const playNextTrack = () => setTrackIndex(( prevIndex ) => ( prevIndex === recommends.length - 1 ? 0 : prevIndex + 1 ));
   const handleProgress = ( state: { played: SetStateAction<number>; }) => {
     setPlayed( state.played );
   };
@@ -113,13 +109,51 @@ const AudioVideoPlayer = ( props: MusicListProps ) => {
     setPlayed( clickPosition );
   };
 
+
+
+  useEffect(() => {
+    // temp에 정보 담고
+    console.log(playList)
+    const temp = [];
+    if (playList && Array.isArray(playList)) {
+      playList.forEach((e) => {
+        if ('uri' in e && 'song_name' in e && 'artist_name_basket' in e) {
+          temp.push({
+            uri: 'https://www.youtube.com/watch?v=' + e.uri,
+            songName: e.song_name,
+            artist: Array.isArray(e.artist_name_basket) ? e.artist_name_basket.join(', ') : e.artist_name_basket
+          });
+        }
+      });
+    }
+    // console.log(temp)
+    setRecommends(temp);
+  }, [playList]);
+
+
+  useEffect(() => {
+    if ( recommends && recommends.length > 0 && recommends[trackIndex] ) {
+      // 현재 트랙 제목 가수 설정
+      const currentTrack = recommends[trackIndex];
+      setTitle( currentTrack.songName ); 
+      setSinger( currentTrack.artist );
+    }
+  }, [trackIndex, recommends]); 
+
+  const getThumbnailUrl = ( url:string ) => {
+    //예외처리
+    if (!url) return "";
+    const videoId = url.split('v=')[1];
+    return `https://img.youtube.com/vi/${ videoId }/default.jpg`;
+  }
+
   return (
     <PlayerWrapper>
-        <Thumbnail src={ getThumbnailUrl( playlist[ trackIndex ])} alt="Video thumbnail" />
+        <Thumbnail src={ getThumbnailUrl( recommends[ trackIndex ]?.uri)} alt="Video thumbnail" />
         <ControlWrapper>
             <div style={{ width: '80%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
-              <span style={{ fontSize: '40px', fontWeight: 'bold', padding: '10px' }}>Title Title Title</span>
-              <span style={{ fontSize: '25px', padding: '10px'  }}>Singer</span>
+              <span style={{ fontSize: '40px', fontWeight: 'bold', padding: '10px' }}>{Title || 'title'}</span>
+              <span style={{ fontSize: '25px', padding: '10px'  }}>{Singer || 'singer'}</span>
             </div>
             <ProgressWrapper onClick={ seekTo }>
                 <ProgressBar style={{ width: `${played * 100}%` }} />
@@ -135,7 +169,7 @@ const AudioVideoPlayer = ( props: MusicListProps ) => {
 
         <ReactPlayer
         ref={ playerRef }
-        url={ playlist[ trackIndex ] }
+        url={ recommends[ trackIndex ]?.uri }
         playing={ playing }
         onProgress={ handleProgress }
         controls
