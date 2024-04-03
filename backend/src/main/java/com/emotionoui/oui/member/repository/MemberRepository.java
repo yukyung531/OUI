b1a4087e733cc9b6907c60d3333cbd88644d355b
정민지 <mjje27@gmail.com>
2024-04-03 14:58:53 +0900

    modify: 감정통계 기능 수정

--- backend/src/main/java/com/emotionoui/oui/member/repository/MemberRepository.java
+++ backend/src/main/java/com/emotionoui/oui/member/repository/MemberRepository.java
@@ -3,6 +3,8 @@ package com.emotionoui.oui.member.repository;
 import com.emotionoui.oui.member.entity.Member;
 import com.emotionoui.oui.schedule.entity.Schedule;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,4 +14,9 @@ import java.util.Optional;
 public interface MemberRepository extends JpaRepository<Member, Integer> {
     Optional<Member> findByEmail(String email);
     Optional<Member> findByMemberIdAndIsDeleted(Integer memberId, Integer isDeleted);
+
+    @Query("SELECT m.nickname FROM Member m WHERE m.memberId = :memberId")
+    String findNicknameById(@Param("memberId") Integer memberId);
 }
+
+
